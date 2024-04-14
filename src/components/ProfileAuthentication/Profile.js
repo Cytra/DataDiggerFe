807ef40d3d0b8b32a478f70f3fbe8dfe54d63fd5
Martynas Samuilovas <martynas.samuilovas@saldo.com>
2024-04-14 23:47:16 +0300

    profile page update

--- src/components/ProfileAuthentication/Profile.js
+++ src/components/ProfileAuthentication/Profile.js
@@ -1,23 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { useUser } from "../Context/userContext";
 
 const Profile = () => {
-    const { user, updateUser } = useUser();
+    const { user, updateUser, logout } = useUser();
+    const [activeSection, setActiveSection] = useState('profileInfo'); // Default active section
 
     const handleUnsubscribe = () => {
         updateUser({ ...user, membershipPlan: null, subscriptionStatus: 'inactive' });
     };
 
+    const handleSectionChange = (section) => {
+        setActiveSection(section);
+    };
+
+    const handleLogout = () => {
+        logout();
+        // Redirect or perform other logout actions as needed
+    };
+
     return (
-        <>
-            <div className="Profile">
-                <h2>Profile</h2>
-                <p>User Email: {user.email}</p>
-                <p>API Key: {user.apiKey}</p>
-                <p>Membership Plan: {user.membershipPlan}</p>
-                <button  type="submit" onClick={handleUnsubscribe}>Unsubscribe</button>
+        <div className="ProfileContainer">
+            <div className="Sidebar">
+                <button onClick={() => handleSectionChange('profileInfo')}>Profile Info</button>
+                {/* <button onClick={() => handleSectionChange('subscriptions')}>Subscriptions</button> */}
+                <button onClick={handleLogout}>Logout</button>
             </div>
-        </>
+            <div className="ProfileContent">
+                {activeSection === 'profileInfo' && (
+                    <div className="Profile">
+                        <h2>Profile</h2>
+                        <p>User Email: {user.email}</p>
+                        <p>API Key: {user.apiKey}</p>
+                        <p>Membership Plan: {user.membershipPlan}</p>
+                        {/* <p>Created: {user.created}</p> */}
+                    </div>
+                )}
+                {/* {activeSection === 'subscriptions' && (
+                    <div className="Subscriptions">
+  
+                    </div>
+                )} */}
+            </div>
+        </div>
     );
 };
 
